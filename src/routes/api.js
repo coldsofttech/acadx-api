c85b078e0ecca21ee5e544b70c36b5943efd9baf
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2024-08-17 19:26:23 +0100

    Introduced "SSO-Integrations" API (#4)

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { getTitles } = require('../services/titles');
+const SsoIntegration = require('../services/sso-integrations');
+
+const ssoIntegration = new SsoIntegration();
+router.get('/sso-integrations', async (req, res) => {
+    try {
+        const ssoData = await ssoIntegration.getSsoIntegrations();
+        res.json(ssoData);
+    } catch (error) {
+        if (error.message.includes('exceeds the limit of 6')) {
+            res.status(406).json({ error: error.message });
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+    }
+});
 
 router.get('/titles', (req, res) => {
     const titles = getTitles();
